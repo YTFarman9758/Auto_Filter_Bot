c9faa618801f4de8aed79aafab5b5df813207a1d
YTFarman9758 <162802755+YTFarman9758@users.noreply.github.com>
2026-03-25 21:47:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -122,7 +122,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+LfMoLOc65BkyOThl') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+YG7jwgqQEdpkNDU1') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Heart_broker0') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/+LfMoLOc65BkyOThl') # Update channel link for the bot
 
